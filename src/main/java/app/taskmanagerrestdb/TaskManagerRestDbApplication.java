db9394afbe6cfa9131b814b9f0c2181a8fbe37d9
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-06-24 23:47:23 +0200

    Init

---
+++ src/main/java/app/taskmanagerrestdb/TaskManagerRestDbApplication.java
@@ -0,0 +1,13 @@
+package app.taskmanagerrestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerRestDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerRestDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/taskmanagerrestdb/TaskManagerRestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package app.taskmanagerrestdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerRestDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
