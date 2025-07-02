1daa4fbdf7dd5b412f3e5f4fa8cf81da3a947631
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-07-02 23:46:53 +0200

    Added dto and entity

---
+++ src/main/java/dto/TaskResponse.java
@@ -0,0 +1,13 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean isCompleted;
+}
---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private Boolean isCompleted;
+}
