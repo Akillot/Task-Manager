aab30eb7d443668d96aecede51578ba0dd51fb80
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-07-06 03:00:33 +0200

    Added service

---
+++ src/main/java/service/TaskService.java
@@ -0,0 +1,85 @@
+package service;
+
+import dto.TaskRequest;
+import dto.TaskResponse;
+import entity.Task;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+import repository.TaskRepo;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskService {
+
+    private final TaskRepo taskRepo;
+
+    public TaskService(TaskRepo taskRepo) {
+        this.taskRepo = taskRepo;
+    }
+
+    public TaskResponse createTask(@NotNull TaskRequest taskRequest) {
+        Task task = new Task();
+        task.setTitle(taskRequest.getTitle());
+        task.setDescription(taskRequest.getDescription());
+        task.setIsCompleted(taskRequest.getIsCompleted());
+
+        Task savedTask = taskRepo.save(task);
+        return mapToResponse(savedTask);
+    }
+
+    public List<TaskResponse> getAllTasks(){
+        return taskRepo.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public TaskResponse getTaskById(Long id){
+        Task task = taskRepo.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("No task with id " + id));
+        return mapToResponse(task);
+    }
+
+    public void deleteTaskById(Long id){
+        if(!taskRepo.existsById(id)) throw new NoSuchElementException("No task with id " + id);
+        taskRepo.deleteById(id);
+    }
+
+    public List<TaskResponse> sortingByStatusFalseFirst(){
+        return taskRepo.findAll().stream()
+                .sorted((t1,t2) -> Boolean.compare(t1.getIsCompleted(), t2.getIsCompleted()))
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public List<TaskResponse> sortingByStatusTrueFirst(){
+        return taskRepo.findAll().stream()
+                .sorted((t1,t2) -> Boolean.compare(t2.getIsCompleted(), t1.getIsCompleted()))
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public TaskResponse updateTask(Long id, @NotNull TaskRequest request){
+        Task task = taskRepo.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("No task with id " + id));
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setIsCompleted(request.getIsCompleted());
+
+        Task updatedTask = taskRepo.save(task);
+        return mapToResponse(updatedTask);
+    }
+
+    private @NotNull TaskResponse mapToResponse(@NotNull Task task){
+        TaskResponse response = new TaskResponse();
+        response.setId(task.getId());
+        response.setTitle(task.getTitle());
+        response.setDescription(task.getDescription());
+        response.setIsCompleted(task.getIsCompleted());
+        return response;
+    }
+}
