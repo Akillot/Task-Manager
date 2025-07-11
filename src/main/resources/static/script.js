09d99568d2d70e761834b9cff915679396496199
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-07-11 16:58:31 +0200

    UI has been updated

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -19,3 +19,35 @@ async function createTask() {
 
     loadTasks();
 }
+
+async function sortTrueFirst(){
+    const res = await fetch('/tasks/sort-true-first');
+    const tasks = await res.json();
+    document.getElementById('output').innerText = JSON.stringify(tasks, null, 2);
+}
+
+async function sortFalseFirst(){
+    const res = await fetch('/tasks/sort-false-first');
+    const tasks = await res.json();
+    document.getElementById('output').innerText = JSON.stringify(tasks, null, 2);
+}
+
+async function deleteTask() {
+    const id = prompt("Enter ID:");
+    if (id === null || id.trim() === "") {
+        alert("ID not found");
+        return;
+    }
+
+    const response = await fetch(`/tasks/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.status === 204) {
+        alert("Task has been deleted");
+    } else {
+        alert("Error");
+    }
+
+    loadTasks();
+}
